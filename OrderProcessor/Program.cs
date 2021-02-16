e4f84b569debcef3c5996afae5942e3899a7db5f
apartridge <alan.partridge@medicalpharmacies.com>
2021-02-16 14:18:44 -0500

    Add project files.

---
+++ OrderProcessor/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace OrderProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			//declare variables
+			string FileHead;
+			string CustNum;
+			string PurcNum;
+			String FileFoot;
+			int Chop;
+
+
+			string orderfile = System.IO.File.ReadAllText(@"C:\Users\alan.partridge\Desktop\Ordering\medis.txt");
+
+			//The variable chop is used to measure out the length until we hit the delimiter + sign, then we can use that number to put substrings into the 
+			//appropriate variables and truncate the orderfile string. This block chops off the File Header
+			Chop = orderfile.IndexOf("+");
+			FileHead = orderfile.Substring(0, Chop);
+			orderfile = orderfile.Remove(0, Chop + 1);
+
+			//This block chops off the customer number string and removes it from the orderfile string
+			Chop = orderfile.IndexOf("+");
+			CustNum = orderfile.Substring(0, Chop);
+			orderfile = orderfile.Remove(0, Chop + 1);
+
+			//This block chops off the purchase order number string and removes it from the orderfile string
+			Chop = orderfile.IndexOf("+");
+			PurcNum = orderfile.Substring(0, Chop);
+			orderfile = orderfile.Remove(0, Chop + 1);
+
+			//Trim whitespace off PO
+			PurcNum = PurcNum.TrimEnd();
+
+			//This block chops off the file footer string and removes it from the orderfile string
+			Chop = orderfile.IndexOf("+AAAAAAAAAA+");
+			FileFoot = orderfile.Remove(0, Chop);
+			orderfile = orderfile.Remove(Chop, 12);
+
+			System.Console.WriteLine(FileHead);
+			System.Console.WriteLine(CustNum);
+			System.Console.WriteLine(PurcNum);
+			System.Console.WriteLine(FileFoot);
+			System.Console.WriteLine(orderfile);
+
+			//Get the number of items
+			int ttlitems;
+			string[] itemcount;
+			itemcount = orderfile.Split("+");
+			ttlitems = (itemcount.Length);
+			System.Console.WriteLine(ttlitems);
+
+			//Additional Notes: If adding an ability to remove or add items from a file, any item search should include the leading + in the search string, in order to ensure
+			//a false early match does not occur with the trailing 5 numbers of an item number followed by the first digit of the quantity performs a false match.
+		}
+	}
+}
